8be63ed7145289d679698c2e51cfe6d350e94a89
sammy508 <chapagainsamir9@gmail.com>
2025-10-01 16:14:30 +0545

    updated

--- portfolio_project/portfolio/models/Profile_model.py
+++ portfolio_project/portfolio/models/Profile_model.py
@@ -6,7 +6,7 @@ class Profile(models.Model):
 
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     bio = models.TextField(blank=True, null=True, max_length=750)
-    profile_picture = models.ImageField(upload_to="profile/",blank=True, null=True)
+    profile_picture = models.ImageField(upload_to="Profile_Image/",blank=True, null=True)
     location = models.CharField(max_length=100, blank=True, null=True)
     dob = models.DateField(null=True, blank=True)
 
